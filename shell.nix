c4c4cfe8b83b9761a6d8ab5c2de1af360deb3bf0
Markus Hauck <markus1189@gmail.com>
2020-12-23 22:56:52 +0100

    Compile instead of interpret

--- shell.nix
+++ shell.nix
@@ -2,6 +2,10 @@
 with pkgs;
 
 let
+  compileHaskell = day: inputFile: pkgs.runCommand "aoc-builder" {} ''
+    mkdir -p $out/bin
+    ${myGhc}/bin/ghc --make -O3 -rtsopts -threaded -o "$out/bin/d${day}" ${inputFile}
+  '';
   myHaskellPackages = ps:
     with ps; [
       array
@@ -21,11 +25,10 @@ let
       text
       unordered-containers
       stm
+      stm-linkedlist
     ];
   myGhc = haskellPackages.ghcWithHoogle myHaskellPackages;
   mkDay = n:
     let n' = if n < 10 then "0${toString n}" else toString n;
-    in writeScriptBin "d${n'}" ''
-      ${myGhc}/bin/runhaskell --ghc-arg=-Wall day${n'}.hs
-    '';
-in mkShell { buildInputs = [ myGhc ] ++ map mkDay (lib.range 1 24); }
+    in compileHaskell n' (./day + "${n'}.hs");
+in mkShell { buildInputs = [ myGhc ] ++ map mkDay (lib.range 1 23); }
