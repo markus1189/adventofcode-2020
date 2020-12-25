1656bdec54a4058ca71741bdf3b1580633a79187
Markus Hauck <markus1189@gmail.com>
2020-12-25 18:37:32 +0100

    Use ST instead of IO for mutability

--- day23.hs
+++ day23.hs
@@ -2,12 +2,14 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 
+import Control.Parallel.Strategies (parMap, rseq)
+import Data.STRef
+import Control.Monad.ST
 import           Control.Monad (void)
 import           Data.Array (Array)
 import qualified Data.Array as Array
 import           Data.Foldable (foldlM)
 import           Data.Functor (($>))
-import           Data.IORef (IORef, newIORef, atomicWriteIORef, readIORef)
 import           Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.List.NonEmpty as NonEmpty
 import           Data.Text (Text)
@@ -15,40 +17,40 @@ import qualified Data.Text.IO as TIO
 import           Text.Parsec (Parsec)
 import qualified Text.Parsec as Parsec
 
-data Cup = Cup { cupLabel :: Int
-               , cupNext :: IORef Cup
-               }
+data Cup s = Cup { cupLabel :: Int
+                 , cupNext :: STRef s (Cup s)
+                 }
 
-mkCup :: Int -> IO Cup
-mkCup lbl = Cup lbl <$> newIORef (error "unset next pointer")
+mkCup :: Int -> ST s (Cup s)
+mkCup lbl = Cup lbl <$> newSTRef (error "unset next pointer")
 
-buildCups :: Traversable f => f Int -> IO (f Cup)
+buildCups :: Traversable f => f Int -> ST s (f (Cup s))
 buildCups = traverse mkCup
 
-linkCups :: NonEmpty Cup -> IO ()
+linkCups :: NonEmpty (Cup s) -> ST s ()
 linkCups (c :| cs) = do
   endOfLinkedCups <- foldlM link c cs
-  atomicWriteIORef (cupNext endOfLinkedCups) c
+  writeSTRef (cupNext endOfLinkedCups) c
 
-link :: Cup -> Cup -> IO Cup
-link acc cup = atomicWriteIORef (cupNext acc) cup $> cup
+link :: Cup s -> Cup s -> ST s (Cup s)
+link acc cup = writeSTRef (cupNext acc) cup $> cup
 
-cupTake :: Int -> Cup -> IO [Int]
+cupTake :: Int -> Cup s -> ST s [Int]
 cupTake 0 _ = pure []
 cupTake n c = do
   next <- nextCup c
   (cupLabel c :) <$> cupTake (n-1) next
 
-nextCup :: Cup -> IO Cup
-nextCup = readIORef . cupNext
+nextCup :: Cup s -> ST s (Cup s)
+nextCup = readSTRef . cupNext
 
 main :: IO ()
 main = do
   input <- TIO.getContents
-  (solvePart1 . parseInput $ input) >>= putStrLn
-  (solvePart2 . parseInput $ input) >>= print
+  putStrLn $ runST (solvePart1 . parseInput $ input)
+  print $ runST (solvePart2 . parseInput $ input)
 
-solvePart1 :: NonEmpty Int -> IO String
+solvePart1 :: NonEmpty Int -> ST s String
 solvePart1 ds = do
   cups <- buildCups ds
   linkCups cups
@@ -57,14 +59,14 @@ solvePart1 ds = do
   genericSolve (NonEmpty.head cups) ary maxLabel 100
   concatMap show . drop 1 <$> cupTake maxLabel (ary Array.! 1)
 
-solvePart2 :: NonEmpty Int -> IO Int
+solvePart2 :: NonEmpty Int -> ST s Int
 solvePart2 prefix = do
   let ds = prefix <> NonEmpty.fromList [10..oneMillion]
       repetitions = 10 * 1000 * 1000
       oneMillion = 1 * 1000 * 1000
   cups <- buildCups ds
   linkCups cups
-  let ary = Array.array (1, maxLabel) (map (\c -> (cupLabel c, c)) (NonEmpty.toList cups))
+  let ary = Array.array (0, maxLabel+10) (map (\c -> (cupLabel c, c)) (NonEmpty.toList cups))
       maxLabel = oneMillion
       firstCup = ary Array.! 1
   genericSolve (NonEmpty.head cups) ary maxLabel repetitions
@@ -72,7 +74,7 @@ solvePart2 prefix = do
   c2 <- nextCup c1
   pure (cupLabel c1 * cupLabel c2)
 
-genericSolve :: Cup -> Array Int Cup -> Int -> Int -> IO ()
+genericSolve :: Cup s -> Array Int (Cup s) -> Int -> Int -> ST s ()
 genericSolve s ary maxLabel reps = do
   void $ foldlM (\acc _ -> makeMove ary maxLabel acc) s [1..reps]
 
@@ -86,7 +88,7 @@ calculateDestinationCup :: Int -> [Int] -> Int -> Int
 calculateDestinationCup maxLabel removedLabels currentLabel =
   head $ dropWhile (`elem` removedLabels) . drop 1 $ iterate (nextCandidate maxLabel) currentLabel
 
-makeMove :: Array Int Cup -> Int -> Cup -> IO Cup
+makeMove :: Array Int (Cup s) -> Int -> Cup s -> ST s (Cup s)
 makeMove ary maxLabel cup = do
   segment <- cut 3 cup
   xs <- segmentToList segment
@@ -95,7 +97,7 @@ makeMove ary maxLabel cup = do
   paste destinationCup segment
   nextCup cup
 
-segmentToList :: (Cup, Cup) -> IO [Int]
+segmentToList :: (Cup s, Cup s) -> ST s [Int]
 segmentToList (start, end) = do
   if cupLabel start == cupLabel end
   then pure [cupLabel end]
@@ -103,18 +105,18 @@ segmentToList (start, end) = do
     next <- nextCup start
     (cupLabel start :) <$> segmentToList (next, end)
 
-cut :: Int -> Cup -> IO (Cup, Cup)
+cut :: Int -> Cup s -> ST s (Cup s, Cup s)
 cut n cup = do
   lastOfSegment <- foldlM (\cup' _ -> nextCup cup') cup (replicate n ())
   headOfSegment <- nextCup cup
-  nextCup lastOfSegment  >>= atomicWriteIORef (cupNext cup)
+  nextCup lastOfSegment  >>= writeSTRef (cupNext cup)
   pure (headOfSegment, lastOfSegment)
 
-paste :: Cup -> (Cup, Cup) -> IO ()
+paste :: Cup s -> (Cup s, Cup s) -> ST s ()
 paste targetCup (headOfSegment, lastOfSegment) = do
   next <- nextCup targetCup
-  atomicWriteIORef (cupNext targetCup) headOfSegment
-  atomicWriteIORef (cupNext lastOfSegment) next
+  writeSTRef (cupNext targetCup) headOfSegment
+  writeSTRef (cupNext lastOfSegment) next
 
 parseInput :: Text -> NonEmpty Int
 parseInput input =
